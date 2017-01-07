beae19d071c9d4289a751ecab6afacc562729ab6
kockabence <bence.bartho@gmail.com>
2017-01-07 19:25:43 +0100

    step 1: simple edge detection with opencv

--- canny-edge.py
+++ canny-edge.py
@@ -9,5 +9,4 @@ plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
 plt.show()
